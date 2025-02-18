77ef59f2979176a153beec69db8d7c61b08a1104
cs <835>
2025-02-18 16:45:09 +0800

    提交

--- project1/src/com/zuixin/b/String_fangfa.java
+++ project1/src/com/zuixin/b/String_fangfa.java
@@ -43,15 +43,55 @@ public class String_fangfa {
         // String str = sj.toString();
         // System.out.println(str);
         // System.out.println('0'+0);
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        int sum = str_int(str);
-        int A = sum*2;
-        System.out.println(sum);
-        System.out.println(A);
-        
+        // Scanner sc = new Scanner(System.in);
+        // String str = sc.next();
+        // int sum = str_int(str);
+        // int A = sum*2;
+        // System.out.println(sum);
+        // System.out.println(A);
+        // int count = str_chuxian("abc dabefg", "abc ");
+        // System.out.println(count);
+        String str1 = "abcdefcabdijk";
+        String str2 = "ab";
+        // int a = str1.indexOf(str2);
+        // int a = str1.indexOf(str2,3);
+        int a = str_count(str1, str2);
+
+        System.out.println(a);
+
+
+
+
+
+
+
     }
 
+    // 自己写的计算一个字符串在另一个字符串出现的次数
+    public static int str_chuxian(String str, String chuxian_str){
+        String result_str = str.replace(chuxian_str, "1");
+        int count = 0;
+        for (int i = 0; i < result_str.length(); i++) {
+            char A = result_str.charAt(i);
+            if(A == '1'){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // 计算一个字符串在另一个字符串出现的次数       标准的
+    public static int str_count(String str, String str_zhaode){
+        int count = 0;
+        int index;
+        while((index = str.indexOf(str_zhaode)) != -1){
+            str = str.substring(index+str_zhaode.length());
+            count++;
+        }
+        return count;
+    }
+
+    // 字符数字变为int数字
     public static int str_int(String str){
         int sum =0;
         for (int i = 0; i < str.length(); i++) {
